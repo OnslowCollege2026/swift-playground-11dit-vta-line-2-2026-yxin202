15ede10d34a377e5d0bad476d151d2e37d379337
Eric Xing <100739927+yxin202@users.noreply.github.com>
2026-06-11 22:32:53 +0000

    Completed for achieved

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -33,14 +33,15 @@ struct SwiftPlayground {
                 }
                 print("Enter a number: ")
                 let numberSelected: Int = Int(readLine()!)!
+
                 // Get the bird'name from the list based on the number Maia selected
                 let birdMaiaSaw = birdList[numberSelected - 1]
-                speciesMaiaSeen.append(birdMaiaSaw)
 
                 // Check if the specie list contains the bird Maia just saw
                 if speciesMaiaSeen.contains(birdMaiaSaw){
                     // If the specie list contains the bird Maia just saw, increase the total seen time by 1
-                    numberOfSpecies[speciesMaiaSeen.firstIndex(of: birdMaiaSaw)!] += 1
+                    let index = speciesMaiaSeen.firstIndex(of: birdMaiaSaw)!
+                    numberOfSpecies[index] = numberOfSpecies[index] + 1
                     
                 }else{
                     // If the specie list does not contain the bird Maia just saw, add the bird into the
@@ -53,6 +54,7 @@ struct SwiftPlayground {
                 print("Added a \(birdMaiaSaw)")
 
             }else if birdOrInsect?.lowercased() == "insect"{
+
                 // Print out insect list when Maia says she saw a insect and ask her which one did she see
                 print("Which insect did you see?")
                 insectList.enumerated().forEach{ index, insectName in
@@ -60,26 +62,48 @@ struct SwiftPlayground {
                 }
                 print("Enter a number: ")
                 let numberSelected: Int = Int(readLine()!)!
+                // Get the insect'name from the list based on the number Maia selected
                 let insectMaiaSaw = insectList[numberSelected - 1]
-                speciesMaiaSeen.append(insectMaiaSaw)
+
+                // Check if the specie list contains the insect Maia just saw
+                if speciesMaiaSeen.contains(insectMaiaSaw){
+                    // If the specie list contains the insect Maia just saw, increase the total seen time by 1
+                    let index = speciesMaiaSeen.firstIndex(of: insectMaiaSaw)!
+                    numberOfSpecies[index] = numberOfSpecies[index] + 1
+                    
+                }else{
+                    // If the specie list does not contain the insect Maia just saw, add the insect into the
+                    // speice list and set the seen time to 1
+                    speciesMaiaSeen.append(insectMaiaSaw)
+                    numberOfSpecies.append(1)
+                }
+
+                // Tell Maia the insect is added
                 print("Added a \(insectMaiaSaw)")
+
             }
             // Ask Maia if she wants to add another specie, "y" for continue and "n" for stop
             print("Do you want to add another (Y/N): ")
             let userDecision = readLine()
+            // If yes, the programme keep asking Maia
             if userDecision?.lowercased() == "y"{
-                isAdding = true
-            }else if userDecision?.lowercased() == "n"{
+                isAdding = true}
+            // If not, it counts the species she has seen and shows her the totals
+            else if userDecision?.lowercased() == "n"{
                 isAdding = false
             }
         }
+        // Bind together the species list and the seen time list to help to print species out 1 by 1
+        let totalSpecies = Array(zip(speciesMaiaSeen, numberOfSpecies))
         print("Today, you saw:")
-        print(numberOfSpecies)
-        print(speciesMaiaSeen)
+        // Get each specie and print out the total seen time of it and its name
+        totalSpecies.forEach{ specie in
+        print("\(specie.1) \(specie.0)")
+        }
         print("Good bye")
     }
 }
 
 
 
-// If not, it counts the species she has seen and shows her the totals
+
