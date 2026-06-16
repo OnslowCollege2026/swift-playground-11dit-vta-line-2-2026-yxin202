50a04044e2baa98128e36b78ee1e837d74476511
Eric Xing <100739927+yxin202@users.noreply.github.com>
2026-06-16 20:44:07 +0000

    finished the task for achieved

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,97 +4,73 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        let birds: [String] = ["Tieke", "Kākā", "Takahē", "Hihi", "Kiwi", "Pāteke", "Tūī", "Kererū"]
-        let insects: [String] = ["Giant Wētā", "Tree Wētā", "Cave Wētā", "Putoko", "Pūngāwerewere", "Ngaokeoke", "Waemano", "Kapowai"]
-        var isRunning = true
-        var isAskingContinue = true
-        var creaturesMaiaSaw: [String] = []
-        var numberOfCreatures: [Int] = []
+
+        // Create menu for food
+        let foodMenu : [String] = ["Burger", "Sushi", "Pizza", "Garlic bread", "Pork bun", "Dumplings"]
+        let foodCost : [Double] = [7.00, 4.00, 5.00, 4.00, 5.00, 5.00]
+
+        // Create menu for drink
+        let drinkMenu : [String] = ["Juice", "Up & Go", "Gatorade", "Water", "Coffee", "Fruit soda drink"]
+        let drinkCost : [Double] = [4.00, 4.50, 6.00, 3.00, 6.00, 4.00]
+
+        // Set up condition for while loop checking if the student want to keep ordering or not
+        var isOrdering = true
+
+        // Set a variable to store total cost of student's order
+        var totalCost : Double = 0
 
 
-        print("Welcome to Zealandia Tracker")
-
-
-        while isRunning{
-            
-            print("Did you see a bird or insect: ")
-            if let creatureType: String = readLine(){
-                if creatureType.lowercased() == "bird"{
-                    print("Which bird did you see?")
-                    birds.enumerated().forEach { index, birdName in
-                        print("\(index + 1). \(birdName)")
-                    }
-                    print("Enter a number: ")
-                    if let userInput = readLine(),
-                    let indexUserEntered = Int(userInput){
-                        if indexUserEntered > 0 && indexUserEntered <= birds.count{
-                            let birdMaiaSaw = birds[indexUserEntered - 1]
-                            if creaturesMaiaSaw.contains(birdMaiaSaw){
-                                numberOfCreatures[creaturesMaiaSaw.firstIndex(of: birdMaiaSaw)!] += 1
-                            }else{
-                                creaturesMaiaSaw.append(birdMaiaSaw)
-                                numberOfCreatures.append(1)
-                            }
-                            print("\(birdMaiaSaw) is added")
-                            
-                        }else{
-                            print("Out of range number selected!")
-                        }
-                    }else{
-                        print("Please enter a number")
-                    }
-                }else if creatureType.lowercased() == "insect"{
-                    print("Which insect did you see?")
-                    insects.enumerated().forEach { index, insectName in
-                        print("\(index + 1). \(insectName)")
-                    }
-                    print("Enter a number: ")
-                    if let userInput = readLine(),
-                    let indexUserEntered = Int(userInput){
-                        if indexUserEntered > 0 && indexUserEntered <= insects.count{
-                            let insectMaiaSaw = insects[indexUserEntered - 1]
-                            if creaturesMaiaSaw.contains(insectMaiaSaw){
-                                numberOfCreatures[creaturesMaiaSaw.firstIndex(of: insectMaiaSaw)!] += 1
-                            }else{
-                                creaturesMaiaSaw.append(insectMaiaSaw)
-                                numberOfCreatures.append(1)
-                            }
-                            print("\(insectMaiaSaw) is added")
-                        }else{
-                            print("Out of range number selected!")
-                        }
-                    }else{
-                        print("Please enter a number")
-                    }
-                }else{
-                    print("Please select bird or insect")
+        //  The welcoming message
+        print("Welcome to Canteen.")
+        
+        while isOrdering{
+            // Asks students if they want a food or a drink
+            print("Do you want to buy a food or a drink: ")
+            let foodOrDrink = readLine()?.lowercased()
+            if foodOrDrink == "food"{
+                // Asks them to select the food they want from the menu (slide 10)
+                print("What do you want to order?")
+                (0...foodMenu.count - 1).forEach { index in
+                    print("\(index + 1). \(foodMenu[index]) - $\(foodCost[index])")
                 }
-                isAskingContinue = true
-                while isAskingContinue{
-                    print("Do you want to add another (Y/N): ")
-                    if let userDecision = readLine()?.lowercased(){
-                        if userDecision == "y"{
-                            isRunning = true
-                            isAskingContinue = false
-                        }else if userDecision == "n"{
-                            isRunning = false
-                            isAskingContinue = false
-                        }else{
-                            print("Please enter Y/N")
-                        }
-                    }else{
-                        print("Please enter a valid input")
-                    }
+                print("Enter a number: ")
+                let userInput : String = readLine()!
+                let indexUserChose : Int = Int(userInput)!
+                let foodName = foodMenu[indexUserChose - 1]
+                let foodCost = foodCost[indexUserChose - 1]
+                // Tell the students that the food is added and add the cost to the total cost
+                print("You ordered \(foodName), it costs $\(foodCost)")
+                totalCost = totalCost + foodCost
+            }
+            else if foodOrDrink == "drink"{
+                // Asks them to select the drink they want from the menu (slide 10)
+                print("What do you want to order?")
+                (0...drinkMenu.count - 1).forEach { index in
+                    print("\(index + 1). \(drinkMenu[index]) - $\(drinkCost[index])")
                 }
-            }else{
-                print("Please enter a valid word")
+                print("Enter a number: ")
+                let userInput : String = readLine()!
+                let indexUserChose : Int = Int(userInput)!
+                let drinkName = drinkMenu[indexUserChose - 1]
+                let drinkCost = drinkCost[indexUserChose - 1]
+                // Tell the students that the drink is added and add the cost to the total cost
+                print("You ordered \(drinkName), it costs $\(drinkCost)")
+                totalCost = totalCost + drinkCost
+            }
+            // Asks them if they want to buy anything else
+            print("Do you want to order something else(Y/N): ")
+            let userDecision = readLine()?.lowercased()
+            if userDecision == "y"{
+                isOrdering = true
+            }
+            else if userDecision == "n"{
+                isOrdering = false
             }
         }
-        let totalList = Array(zip(creaturesMaiaSaw, numberOfCreatures))
-        print("Today you saw:")
-        totalList.forEach { creature in
-        print("\(creature.1) \(creature.0)")
-        }        
+        // If not, it counts the total cost and show it to the students.
+        print("Total cost is $\(totalCost), thank you very much. Have a nice day!")
     }
 }
+
+
 
