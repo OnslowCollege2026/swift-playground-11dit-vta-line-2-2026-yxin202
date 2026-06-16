48275e9728a3b90b9e06eadecacb4dd4c29335e9
Eric Xing <100739927+yxin202@users.noreply.github.com>
2026-06-16 23:44:00 +0000

    tried the task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,97 +4,23 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        let birds: [String] = ["Tieke", "Kākā", "Takahē", "Hihi", "Kiwi", "Pāteke", "Tūī", "Kererū"]
-        let insects: [String] = ["Giant Wētā", "Tree Wētā", "Cave Wētā", "Putoko", "Pūngāwerewere", "Ngaokeoke", "Waemano", "Kapowai"]
-        var isRunning = true
-        var isAskingContinue = true
-        var creaturesMaiaSaw: [String] = []
-        var numberOfCreatures: [Int] = []
-
-
-        print("Welcome to Zealandia Tracker")
-
-
-        while isRunning{
-            
-            print("Did you see a bird or insect: ")
-            if let creatureType: String = readLine(){
-                if creatureType.lowercased() == "bird"{
-                    print("Which bird did you see?")
-                    birds.enumerated().forEach { index, birdName in
-                        print("\(index + 1). \(birdName)")
-                    }
-                    print("Enter a number: ")
-                    if let userInput = readLine(),
-                    let indexUserEntered = Int(userInput){
-                        if indexUserEntered > 0 && indexUserEntered <= birds.count{
-                            let birdMaiaSaw = birds[indexUserEntered - 1]
-                            if creaturesMaiaSaw.contains(birdMaiaSaw){
-                                numberOfCreatures[creaturesMaiaSaw.firstIndex(of: birdMaiaSaw)!] += 1
-                            }else{
-                                creaturesMaiaSaw.append(birdMaiaSaw)
-                                numberOfCreatures.append(1)
-                            }
-                            print("\(birdMaiaSaw) is added")
-                            
-                        }else{
-                            print("Out of range number selected!")
-                        }
-                    }else{
-                        print("Please enter a number")
-                    }
-                }else if creatureType.lowercased() == "insect"{
-                    print("Which insect did you see?")
-                    insects.enumerated().forEach { index, insectName in
-                        print("\(index + 1). \(insectName)")
-                    }
-                    print("Enter a number: ")
-                    if let userInput = readLine(),
-                    let indexUserEntered = Int(userInput){
-                        if indexUserEntered > 0 && indexUserEntered <= insects.count{
-                            let insectMaiaSaw = insects[indexUserEntered - 1]
-                            if creaturesMaiaSaw.contains(insectMaiaSaw){
-                                numberOfCreatures[creaturesMaiaSaw.firstIndex(of: insectMaiaSaw)!] += 1
-                            }else{
-                                creaturesMaiaSaw.append(insectMaiaSaw)
-                                numberOfCreatures.append(1)
-                            }
-                            print("\(insectMaiaSaw) is added")
-                        }else{
-                            print("Out of range number selected!")
-                        }
-                    }else{
-                        print("Please enter a number")
-                    }
-                }else{
-                    print("Please select bird or insect")
-                }
-                isAskingContinue = true
-                while isAskingContinue{
-                    print("Do you want to add another (Y/N): ")
-                    if let userDecision = readLine()?.lowercased(){
-                        if userDecision == "y"{
-                            isRunning = true
-                            isAskingContinue = false
-                        }else if userDecision == "n"{
-                            isRunning = false
-                            isAskingContinue = false
-                        }else{
-                            print("Please enter Y/N")
-                        }
-                    }else{
-                        print("Please enter a valid input")
-                    }
-                }
-            }else{
-                print("Please enter a valid word")
-            }
+        let daysInWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday"]
+        var totalHours : Double = 0
+        var averageHours : Double = 0
+        print("Welecome to sleep tracker")
+        print("")
+        print("Let’s start by recording your sleep.")
+        (0..<daysInWeek.count - 1).forEach{ index in
+            print("What time did you go to sleep on \(daysInWeek[index])?")
+            let sleepTime = Double(readLine()!)
+            print("What time did you wake up on \(daysInWeek[index + 1])?")
+            let wakeTime = Double(readLine()!)
+            let timeSleeping = 12 - sleepTime! + wakeTime!
+            totalHours += timeSleeping
         }
-        let totalList = Array(zip(creaturesMaiaSaw, numberOfCreatures))
-        print("Today you saw:")
-        totalList.forEach { creature in
-        print("\(creature.1) \(creature.0)")
-        }        
+        averageHours = totalHours / 7
+        print("You got total of \(totalHours) hours of sleep this week, which averages to \(averageHours) per day.")
+        
+        
     }
-}
-
+    }
